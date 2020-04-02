400562d7dae64eb8551ded227bbebeaf1ae8f2c2
Mateusz Gliwka <gliwson@gmail.com>
2020-04-02 15:06:41 +0200

    initialize maps

--- src/main/java/pl/management/ManagementApplication.java
+++ src/main/java/pl/management/ManagementApplication.java
@@ -2,15 +2,22 @@ package pl.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ManagementApplication {
+public class ManagementApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ManagementApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ManagementApplication.class);
     }
 
     @Bean
