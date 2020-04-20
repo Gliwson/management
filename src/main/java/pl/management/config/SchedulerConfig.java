d3f9e1fbfefe5a5793069beeb3b5e0c739793b75
Mateusz Gliwka <gliwson@gmail.com>
2020-04-20 20:49:56 +0200

    added config scheduled

---
+++ src/main/java/pl/management/config/SchedulerConfig.java
@@ -0,0 +1,24 @@
+package pl.management.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import pl.management.map.service.schedul.json.ImportSheetsGoogleJson;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    private final ImportSheetsGoogleJson importSheetsGoogleJson;
+
+    public SchedulerConfig(ImportSheetsGoogleJson importSheetsGoogleJson) {
+        this.importSheetsGoogleJson = importSheetsGoogleJson;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.addCronTask(importSheetsGoogleJson::getJson2, "0 0/1 6-22 * * *");
+
+    }
+}
--- src/main/java/pl/management/map/service/schedul/json/ImportSheetsGoogleJson.java
+++ src/main/java/pl/management/map/service/schedul/json/ImportSheetsGoogleJson.java
@@ -6,7 +6,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.management.map.service.dto.ListJsonDTO;
@@ -28,6 +27,7 @@ public class ImportSheetsGoogleJson {
         this.mapperJsonToPointDto = mapperJsonToPointDto;
     }
 
+
     public List<RowDTO> getJson() {
         RestTemplate restTemplate = new RestTemplate();
         String stringJson = restTemplate.getForObject(URL_JSON, String.class);
@@ -37,12 +37,9 @@ public class ImportSheetsGoogleJson {
     }
 
     @EventListener(ApplicationReadyEvent.class)
-    @Scheduled(cron = "50 * 6-20 * * *")
     public List<PointDTO> getJson2() {
         log.info("Execute");
         List<RowDTO> json = getJson();
         return mapperJsonToPointDto.map(json);
     }
-
-
 }
