08f65bd0836bf5804f7e917baebc3c508e0b18c0
Mateusz Gliwka <gliwson@gmail.com>
2020-04-20 19:00:16 +0200

    reformat ImportSheetsGoogleJson

--- src/main/java/pl/management/map/domainmodel/TaskRepository.java
+++ src/main/java/pl/management/map/domainmodel/TaskRepository.java
@@ -1,10 +1,6 @@
 package pl.management.map.domainmodel;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository {
 
 
 }
